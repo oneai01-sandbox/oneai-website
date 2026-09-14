d16797f7a590b2b7a205780bb429cf0a448ac826
doorooful <codingdooroo@gmail.com>
2026-09-14 16:56:44 +0900

    updated upon build config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,10 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  allowedDevOrigins: ["218.159.149.13"],
 };
-
-module.exports = {
-  allowedDevOrigins: ['218.159.149.13'],
-}
 
 export default nextConfig;
